88a672a49e61aef00e9699b56c0d48df951f531a
laikemariam1221 <laikemariam1221@example.com>
2025-07-21 15:00:37 +0300

    Add Product List component

---
+++ ecommerce/src/components/ProductList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Product List</h2>
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default { name: 'ProductList' }
+</script>
