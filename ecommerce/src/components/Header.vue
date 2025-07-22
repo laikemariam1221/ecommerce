c3e86d7159eea53b5d02427422e3acb5619ca870
laikemariam1221 <laikemariam1221@example.com>
2025-07-22 11:49:26 +0300

    Update App.vue and cleanup components

--- ecommerce/src/components/Header.vue
+++ ecommerce/src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="bg-gray-900 text-white p-4">E-Commerce Header</header>
 </template>
+
 <script>
-export default { name: 'Header' }
+export default {
+  name: 'Header'
+}
 </script>
