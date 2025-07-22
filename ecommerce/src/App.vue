98aa1ae3ba6d3067ab33c0be0506b373ccb06a2d
laikemariam1221 <laikemariam1221@example.com>
2025-07-22 11:31:56 +0300

    add navbar

--- ecommerce/src/App.vue
+++ ecommerce/src/App.vue
@@ -1,19 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+import ProductList from './components/ProductList.vue'
+import Navbar from './components/Navbar.vue'
+
 </script>
-
-<template>
+   
+<template> 
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+ <Navbar/>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+     <Header/>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <ProductList />
   </main>
 </template>
 
---
+++ ecommerce/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="bg-blue-600 text-white p-4">
+    <ul class="flex gap-4">
+      <li>Home</li>
+      <li>Shop</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default { name: 'Navbar' }
+</script>
